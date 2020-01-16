43048879468949004964ea804edb7f6e885dc425
Anne _ <anne1993@live.nl>
2020-01-16 15:27:02 +0100

    Dikke poepchinezen

--- BierWeerPoging2/KaartFunction.cs
+++ BierWeerPoging2/KaartFunction.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
+using SkiaSharp;
 
 namespace BierWeerPoging2
 {
@@ -37,8 +38,7 @@ namespace BierWeerPoging2
                 {
                     string textToWrite = GenerateBeerText(weatherRoot);
 
-                    ImageTextWriter imageTextWriter = new ImageTextWriter();
-                    Stream renderedImage = imageTextWriter.WriteTextOnImage(responseContent, textToWrite);
+                    Stream renderedImage = WriteTextOnImage(responseContent, textToWrite);
                 }
                 catch
                 {
@@ -55,8 +55,7 @@ namespace BierWeerPoging2
                 {
                     string textToWrite = GenerateBeerText(weatherRoot);
 
-                    ImageTextWriter imageTextWriter = new ImageTextWriter();
-                    Stream renderedImage = imageTextWriter.WriteTextOnImage(responseContent, textToWrite);
+                    Stream renderedImage = WriteTextOnImage(responseContent, textToWrite);
                     await cloudBlockBlob.UploadFromStreamAsync(renderedImage);
                 }
                 catch
@@ -81,8 +80,7 @@ namespace BierWeerPoging2
                     {
                         string textToWrite = e.Message.ToString();
 
-                        ImageTextWriter imageTextWriter = new ImageTextWriter();
-                        Stream renderedImage = imageTextWriter.WriteTextOnImage(stream, textToWrite);
+                        Stream renderedImage = WriteTextOnImage(stream, textToWrite);
                         await cloudBlockBlob.UploadFromStreamAsync(renderedImage);
                     }
                 }
@@ -111,6 +109,36 @@ namespace BierWeerPoging2
                 maintext = "Wat een lekker weer voor een biertje";
             }
             return maintext;
+        }
+        private static Stream WriteTextOnImage(Stream responseContent, string textToWrite)
+        {
+            ///oh nee system.drawing is niet iets wat we accepteren in azure functions
+            ///sixlaborers.imagesharp kan wel volgens internet advies
+            ///dat werkt ook niet
+            ///beter werkt skiasharp
+            ///eureka
+
+            //maak canvasvanimage en plaats teksten
+            SKBitmap sKBitmap = SKBitmap.Decode(responseContent);
+            SKCanvas canvas = new SKCanvas(sKBitmap);
+
+
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.Color = SKColors.Blue;
+                paint.TextAlign = SKTextAlign.Center;
+                paint.TextSize = 16;
+
+                canvas.DrawText(textToWrite, 250, 50, paint);
+            }
+
+            //vertaal die canvasimage/bitmap weer naar een stream
+            SKBitmap bitmap = sKBitmap;
+            SKImage image = SKImage.FromBitmap(bitmap);
+            SKData encoded = image.Encode();
+            Stream stream = encoded.AsStream();
+
+            return stream;
         }
     }
 }
